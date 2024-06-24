122a67462cb554ed2fd111e8f813a9d7cc5777f2
Ruoxue Wang <ruoxue.wang@outlook.com>
2024-06-24 20:21:26 +0100

    Improved robot in main.py by allowing robot to also have a direction, initialised by the user. The robot will move one step forward in the direction it is facing.

--- main.py
+++ main.py
@@ -4,23 +4,97 @@ identifier = 2628
 row_co = int(input('What is its current row coordinate?'))
 col_co = int(input('What is its current column coordinate?'))
 grid_size = 10
+max_row = grid_size - 1
+max_col = grid_size - 1
+
+init_dir = input('What is its initial direction [n|s|e|w]?')
 
 print(f'Hello. My name is {name}. My ID is {identifier}.')
+
 if row_co < 0:
     row_co = 0
-elif row_co > grid_size - 1:
-    row_co = grid_size - 1
+elif row_co > max_row:
+    row_co = max_row
 if col_co < 0:
     col_co = 0
-elif col_co > grid_size - 1:
-    col_co = grid_size - 1
-if 0 <= row_co <= 4:
-    if 0 <= col_co <= 4:
+elif col_co > max_col:
+    col_co = max_col
+if 0 <= row_co <= max_row // 2:
+    if 0 <= col_co <= max_col // 2:
         print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
-    elif 5 <= col_co <=9:
+    elif 5 <= col_co <= max_col:
         print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
-elif 5 <= row_co <= 9:
-    if 0 <= col_co <= 4:
+elif 5 <= row_co <= max_row:
+    if 0 <= col_co <= max_col // 2:
         print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
-    elif 5 <= col_co <=9:
+    elif 5 <= col_co <= max_col:
         print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+
+if init_dir == 'n':
+    print('I am facing North.')
+    print('Moving one step forward.')
+    row_co -= 1
+    if row_co > max_row:
+        row_co = max_row
+    if 0 <= row_co <= max_row // 2:
+        if 0 <= col_co <= max_col // 2:
+            print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
+        elif 5 <= col_co <= max_col:
+            print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
+    elif 5 <= row_co <= max_row:
+        if 0 <= col_co <= max_col // 2:
+            print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
+        elif 5 <= col_co <= max_col:
+            print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+
+
+if init_dir == 's':
+    print('I am facing South.')
+    print('Moving one step forward.')
+    row_co += 1
+    if row_co > max_row:
+        row_co = max_row
+    if 0 <= row_co <= max_row // 2:
+        if 0 <= col_co <= max_col // 2:
+            print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
+        elif 5 <= col_co <= max_col:
+            print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
+    elif 5 <= row_co <= max_row:
+        if 0 <= col_co <= max_col // 2:
+            print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
+        elif 5 <= col_co <= max_col:
+            print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+
+if init_dir == 'e':
+    print('I am facing East.')
+    print('Moving one step forward.')
+    col_co += 1
+    if col_co > max_col:
+        col_co = max_col
+    if 0 <= row_co <= max_row // 2:
+        if 0 <= col_co <= max_col // 2:
+            print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
+        elif 5 <= col_co <= max_col:
+            print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
+    elif 5 <= row_co <= max_row:
+        if 0 <= col_co <= max_col // 2:
+            print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
+        elif 5 <= col_co <= max_col:
+            print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+
+if init_dir == 'w':
+    print('I am facing West.')
+    print('Moving one step forward.')
+    col_co -= 1
+    if col_co > max_col:
+        col_co = max_col
+    if 0 <= row_co <= max_row // 2:
+        if 0 <= col_co <= max_col // 2:
+            print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
+        elif 5 <= col_co <= max_col:
+            print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
+    elif 5 <= row_co <= max_row:
+        if 0 <= col_co <= max_col // 2:
+            print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
+        elif 5 <= col_co <= max_col:
+            print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
