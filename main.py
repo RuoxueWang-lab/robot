c6e9016b6f800d513e1de52f3d533e2dd956e603
Ruoxue Wang <ruoxue.wang@outlook.com>
2024-06-28 18:32:38 +0800

    Documenting my python codes and modify my codes to a better version according to the professor's codes.

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+"""
+My amazing robot!
+"""
+
 import random
 
+# Get the robot's name from the user
 name = input('What is the name of the robot?')
+
+# Set ID for the robot
 identifier = 2628
 
+# Maximum number of grid cells (rows and columns)
 grid_size = 10
+
+# Maximum row and column indices
 max_row = grid_size - 1
 max_col = grid_size - 1
+
+# Generate random initial coordinates for the robot
 row_co = random.randint(0, max_row)
 col_co = random.randint(0, max_col)
 
+# Generate a random moving direction for the robot
 init_dir = random.choice(['n','s','w','e'])
 
-print(f'Hello. My name is {name}. My ID is {identifier}.')
-
+# Clip the coordinates to be inside the grid
 if row_co < 0:
     row_co = 0
 elif row_co > max_row:
@@ -21,82 +33,63 @@ if col_co < 0:
     col_co = 0
 elif col_co > max_col:
     col_co = max_col
+
+# Compute the quadrant
 if 0 <= row_co <= max_row // 2:
-    if 0 <= col_co <= max_col // 2:
-        print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
-    elif 5 <= col_co <= max_col:
-        print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
-elif 5 <= row_co <= max_row:
-    if 0 <= col_co <= max_col // 2:
-        print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
-    elif 5 <= col_co <= max_col:
-        print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+    row_quadrant = "top"
+else:
+    row_quadrant = "bottom"
 
-if init_dir == 'n':
-    print('I am facing North.')
-    print('Moving one step forward.')
-    row_co -= 1
-    if row_co > max_row:
-        row_co = max_row
-    if 0 <= row_co <= max_row // 2:
-        if 0 <= col_co <= max_col // 2:
-            print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
-        elif 5 <= col_co <= max_col:
-            print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
-    elif 5 <= row_co <= max_row:
-        if 0 <= col_co <= max_col // 2:
-            print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
-        elif 5 <= col_co <= max_col:
-            print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+if 0 <= col_co <= max_col // 2:
+    col_quadrant = "left"
+else:
+    col_quadrant = "right"
 
+quadrant = f"{row_quadrant} {col_quadrant}"
 
-if init_dir == 's':
-    print('I am facing South.')
-    print('Moving one step forward.')
-    row_co += 1
-    if row_co > max_row:
-        row_co = max_row
-    if 0 <= row_co <= max_row // 2:
-        if 0 <= col_co <= max_col // 2:
-            print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
-        elif 5 <= col_co <= max_col:
-            print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
-    elif 5 <= row_co <= max_row:
-        if 0 <= col_co <= max_col // 2:
-            print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
-        elif 5 <= col_co <= max_col:
-            print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+# Generate a string representation for the direction, for printing
+if init_dir == "n":
+    direction_string = "North"
+elif init_dir == "s":
+    direction_string = "South"
+elif init_dir == "e":
+    direction_string = "East"
+elif init_dir == "w":
+    direction_string = "West"
 
-if init_dir == 'e':
-    print('I am facing East.')
-    print('Moving one step forward.')
-    col_co += 1
-    if col_co > max_col:
-        col_co = max_col
-    if 0 <= row_co <= max_row // 2:
-        if 0 <= col_co <= max_col // 2:
-            print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
-        elif 5 <= col_co <= max_col:
-            print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
-    elif 5 <= row_co <= max_row:
-        if 0 <= col_co <= max_col // 2:
-            print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
-        elif 5 <= col_co <= max_col:
-            print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+# The robot talks!
+print(f"Hello. My name is {name}. My ID is {identifier}.")
+print(f"My current location is ({row_co}, {col_co}). I am in the {quadrant} quadrant.")
+print(f"I am facing {direction_string}.")
 
-if init_dir == 'w':
-    print('I am facing West.')
-    print('Moving one step forward.')
-    col_co -= 1
-    if col_co > max_col:
-        col_co = max_col
-    if 0 <= row_co <= max_row // 2:
-        if 0 <= col_co <= max_col // 2:
-            print(f'My curren location is ({row_co},{col_co}). I am in the top left quadrant.')
-        elif 5 <= col_co <= max_col:
-            print(f'My curren location is ({row_co},{col_co}). I am in the top right quadrant.')
-    elif 5 <= row_co <= max_row:
-        if 0 <= col_co <= max_col // 2:
-            print(f'My curren location is ({row_co},{col_co}). I am in the bottom left quadrant.')
-        elif 5 <= col_co <= max_col:
-            print(f'My curren location is ({row_co},{col_co}). I am in the bottom right quadrant.')
+# Move the robot one step forward
+print("Moving one step forward.")
+
+if init_dir == "n":
+    if row_co > 0:
+        row_co = row_co - 1
+if init_dir == "s":
+    if row_co < grid_size - 1:
+        row_co = row_co + 1
+if init_dir == "w":
+    if col_co > 0:
+        col_co = col_co - 1
+if init_dir == "e":
+    if col_co < grid_size - 1:
+        col_co = col_co + 1
+
+# Update the quadrant
+if 0 <= row_co <= max_row // 2:
+    row_quadrant = "top"
+else:
+    row_quadrant = "bottom"
+
+if 0 <= col_co <= max_col // 2:
+    col_quadrant = "left"
+else:
+    col_quadrant = "right"
+
+quadrant = f"{row_quadrant} {col_quadrant}"
+
+# Print the updated location and quadrant
+print(f"My current location is ({row_co}, {col_co}). I am in the {quadrant} quadrant.")
